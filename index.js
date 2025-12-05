56bcc71c7562840e87f683492b7e2c4e2115f0a0
Nicholas Cruz <ncruz@ntracts.com>
2025-12-05 14:18:06 -0500

    Add cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
+    console.log("Received request at Service A");
   res.json({
     message: "Shipping to production is a spiritual discipline."
   });
